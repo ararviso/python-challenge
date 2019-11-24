5afe55703ca22e7a58c77a23c635b6ab120d2374
ararviso <ararviso@aol.com>
2019-11-23 21:57:58 -0600

    submitting homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,61 @@
-#Start homework
+
+
+import pandas as pd
+import csv
+import os
+
+
+
+#path
+csv_path ='budget_data.csv'
+
+
+
+#read csv
+py_bank_df = pd.read_csv(csv_path)
+
+py_bank_df.head()
+
+
+
+#get total months
+month_count = py_bank_df['Date'].count()
+month_count
+
+
+
+#total revenue(profit/loss)
+total_profit_loss = py_bank_df['Profit/Losses'].sum()
+total_profit_loss
+
+
+
+#average change of profit
+Profit_Loss_Diff = py_bank_df["Profit/Losses"].diff().mean()
+
+Profit_Loss_Diff
+
+
+#greatest increase and decrease 
+
+greatest_increase = py_bank_df["Profit/Loss Diff"].max()
+greatest_decrease = py_bank_df["Profit/Loss Diff"].min()
+
+greatest_increase_row = py_bank_df.loc[py_bank_df["Profit/Loss Diff"]==greatest_increase,"Date"]
+greatest_decrease_row = py_bank_df.loc[py_bank_df["Profit/Loss Diff"]==greatest_decrease,"Date"]
+
+greatest_decrease_date = greatest_decrease_row.iloc[0]
+greatest_increase_date = greatest_increase_row.iloc[0]
+
+print(greatest_decrease_date, greatest_increase_date)
+
+
+
+print("Financial Analysis")
+print("--------------------------------")
+print(f"Total Months: {month_count}")
+print(f"Total Revenue: ${total_profit_loss}")
+print(f"Average Revenue Change: ${Profit_Loss_Diff}")
+print(f"Greatest Increase: {greatest_increase}({greatest_increase_date})")
+print(f"Greatest Decrease: {greatest_decrease}({greatest_decrease_date})")
+
--- PyPol/main.py
+++ PyPol/main.py
@@ -1,1 +1,54 @@
-#Start homework
+
+import pandas as pd
+
+
+
+#path
+csv_path ='election_data.csv'
+
+
+#read csv
+py_poll_df = pd.read_csv(csv_path)
+
+py_poll_df.head()
+
+
+
+#The total number of votes cast
+vote_counts = py_poll_df["Voter ID"].count()
+vote_counts
+
+
+
+#A complete list of candidates who received vote
+candidate = py_poll_df["Candidate"].unique()
+candidate
+
+
+
+#The total number of votes each candidate won
+candidate_counts = py_poll_df["Candidate"].value_counts()
+candidate_counts.head()
+
+
+
+#percentage votes for each candidate
+#percent_of_votes = {candidate_counts}/{vote_counts} * 100
+#print(f"{candidate}: {int(percent_of_votes)}% {votes_counts}")
+
+
+
+#The winner of the election based on popular vote
+winner = py_poll_df["Candidate"].max()
+winner
+
+
+
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes:{vote_counts}")
+print("-------------------------")
+print(f"candidate:{candidate_counts}")
+print(f"Winner: {winner}")
+print("--------------------------")
+
